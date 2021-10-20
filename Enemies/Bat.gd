5cadcfd608afe0ae60786ea704f1e0e59c4440e2
Javakryak <62621778+Javakryak@users.noreply.github.com>
2021-10-20 22:16:05 +0300

    enemy-wander-state

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -20,9 +20,12 @@ onready var stats = $Stats
 onready var animatedSprite = $AnimatedSprite
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
+onready var wanderController = $WanderController
 
 func _ready():
-	start_frame()
+	set_random_start_frame()
+	state = pick_random_state([IDLE, WANDER])
+
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -33,28 +36,39 @@ func _physics_process(delta):
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
 			
+			if wanderController.get_time_left() == 0:
+				state = pick_random_state([IDLE, WANDER])
+				wanderController.start_wander_timer(rand_range(1, 3))
+			
+			
 		WANDER:
-			pass
+			seek_player()
+			if wanderController.get_time_left() == 0:
+				state = pick_random_state([IDLE, WANDER])
+				wanderController.start_wander_timer(rand_range(1, 3))
+			accelerate_towards(delta, wanderController.target_position)
+
 			
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
-				var direction = position.direction_to(player.global_position)
-				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				accelerate_towards(delta, player.global_position)
 			else:
 				state = IDLE
-			animatedSprite.flip_h = velocity.x < 0
 			
 	velocity = move_and_slide(velocity)
+
 
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
 
+
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
 	hurtbox.create_hit_effect()
+
 
 func _on_Stats_no_health():
 	queue_free()
@@ -62,7 +76,20 @@ func _on_Stats_no_health():
 	get_parent().add_child(enemyDeathEffect)
 	enemyDeathEffect.position = position
 
-func start_frame():
+
+func set_random_start_frame():
 	var spriteFrames = animatedSprite.get_sprite_frames()
 	var spriteCount = spriteFrames.get_frame_count(animatedSprite.animation) - 1
 	animatedSprite.frame = rand_range(0, spriteCount)
+
+
+func pick_random_state(state_list):
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	return rng.randi_range(0, len(state_list) - 1)
+
+
+func accelerate_towards(delta, position):
+	var direction = position.direction_to(position)
+	velocity = velocity.move_toward(global_position.direction_to(wanderController.target_position) * MAX_SPEED, global_position.distance_to(wanderController.target_position) * delta)
+	animatedSprite.flip_h = velocity.x < 0
